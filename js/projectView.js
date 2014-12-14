24a60b483532d885e7b9d765fba6939ae44ead18
gmenih341 <gmenih@gmail.com>
2014-12-14 18:06:57 +0100

    Removing functionality - to be fixed

--- js/projectView.js
+++ js/projectView.js
@@ -1,19 +1,27 @@
+var hiddenTasks = [];
 $("#tasksList").children().on("click", function(){
 	$(this).animate({"width": 0, "height": 0, "visibility":"hidden"}, 320, "linear", function(){
-		var task = $(this);
+		hiddenTasks.push($(this));
 		var toast = $("<div>", {"text": "Item removed. Undo", "class":"toastMessage"});
 
 		toast.appendTo(".appContent");
-		var removalTimer = setTimeout(function(){
-			toast.animate({"opacity": 0}, 420, "linear", function(){
-				toast.remove();
-				task.remove();
-			})
-		}, 5000);
+		var removalTimer = timerSetup(toast);
 		toast.on("click", function(){
 			clearTimeout(removalTimer);
+			removalTimer = timerSetup();
 			toast.animate({"opacity":0}, 420, "linear");
-			task.animate({"width": "90%", "height":"50pt", "visibility":"visible"}, 320, "linear");
+			hiddenTasks[0].animate({"width": "90%", "height":"50pt", "visibility":"visible"}, 320, "linear");
+			hiddenTasks.pop();
+
 		})
 	});
 })
+
+function timerSetup(){
+	return setTimeout(function(toast){
+		toast.animate({"opacity": 0}, 420, "linear", function(){
+			toast.remove();
+			hiddenTasks.foreach(function(task){task.remove();});
+		})
+	}, 5000);
+}
