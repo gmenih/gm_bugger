8d6a1b5d409287dca2604d42e2696aca04ffd79a
gmenih341 <gmenih@gmail.com>
2014-12-15 10:23:48 +0100

    Fixed toast display

--- js/projectView.js
+++ js/projectView.js
@@ -4,41 +4,42 @@ var hiddenTasks = []; // list of hidden tasks not yet removed
  * displays a toast message for 5 seconds before fully removing
  * the task.
  */
-$("#tasksList").children().on("click", function(){
-	$(this).hide(420, function(){
+var removalTimer;
+ console.log("someid", $("#someId").length);
+ $("#tasksList").children().on("click", function(){
+ 	$(this).hide(420, function(){
 		var toast; // toast message
 		hiddenTasks.push(this); // add clicked task to array
-		if($("#undoTaskRemove") !== "undefined"){ // if toast not yet displayed, make it happen
+		if($("#undoTaskRemove").length === 0){ // if toast not yet displayed, make it happen
 			toast = $("<div>", {"text": "Item removed. Undo", "class":"toastMessage", "id":"undoTaskRemove"});
 			toast.appendTo(".appContent")
+			console.log("Toast message sent.");
 		}
-		var removalTimer = timerSetup(toast); // start timer
-		toast.on("click", function(){ // if toast message clicked
+		removalTimer = timerSetup(toast); // start timer
+	});
+ })
+$(".appContent").on("click", "#undoTaskRemove", function(){ // if toast message clicked
 			clearTimeout(removalTimer); // restart timer
-			removalTimer = timerSetup(toast);
+			removalTimer = timerSetup(this);
 			// show last task
 			$(hiddenTasks[hiddenTasks.length - 1]).show(420);
 			hiddenTasks.pop(); // remove last task from list
 			if(hiddenTasks.length < 1){
-				toast.remove();
-				toast = null;
+				$(this).remove();
 				clearTimeout(removalTimer);
 			}
 		})	
-	});
-})
-
 /**
  * Timer function, which waits 5 seconds before finally removing all tasks in hiddenTasks array.
  */
-function timerSetup(toast){
-	return setTimeout(function(){
-		console.log("after", toast);
-		toast.animate({"opacity": 0}, 420, "linear", function(){
-			toast.remove();
-			hiddenTasks.forEach(function(task){
-				task.remove();
-			})
-		})
-	}, 5000);
-}
+ function timerSetup(toast){
+ 	return setTimeout(function(){
+ 		console.log("after", toast);
+ 		$(toast).animate({"opacity": 0}, 420, "linear", function(){
+ 			$(toast).remove();
+ 			hiddenTasks.forEach(function(task){
+ 				task.remove();
+ 			})
+ 		})
+ 	}, 5000);
+ }
